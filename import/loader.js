2c80a381505024aeb7433a560193ac7976221ed5
David Lago <davidlago@gmail.com>
2014-05-31 16:59:01 -0400

    Added comments and formatting

--- import/loader.js
+++ import/loader.js
@@ -1,54 +1,56 @@
 var fs  = require("fs")
 var path = require("path")
-var _ = require('underscore');
+var _ = require('underscore')
 
 exports.loadJSON = function (options) {
 
-	var obj = options.json;
-	var intObj = [];
-	var finalObj = [];
-
-	var filename = options.fileroot;
+	var obj = options.json 				// New input file as a JSON
+	var intObj = []						// Intermediate object, built from input JSON
+	var finalObj = []					// Final object, mergin intermediate object and previous state object
+	var filename = options.fileroot		// Input file name (without extension)
 	
+	// Group input document by ORG_CODE
+	var grpORG = _.groupBy(obj, function(el){ return el.ORG_CODE })
 
-	var grpORG = _.groupBy(obj, function(el){ return el.ORG_CODE; });
-
+	// Format the insides of the intermediate object for each grouped ORG_CODE element
 	for (var index in grpORG) {
-
-		var item = grpORG[index];		
-		var tempObj = {};
-		tempObj.ORG_CODE = item[0].ORG_CODE;
+		var item = grpORG[index]		
+		var tempObj = {}
+		tempObj.ORG_CODE = item[0].ORG_CODE
 		tempObj[options.folder] = {}
 		tempObj[options.folder][filename] = item
-		intObj.push(tempObj);
-
+		intObj.push(tempObj)
 	}
 
+	// Merge with previous state document
 	intObj = _.union(intObj,options.state)
 
-	var grpORGT = _.groupBy(intObj, function(el){ return el.ORG_CODE; });
+	// Group merged document by ORG_CODE again
+	var grpORGT = _.groupBy(intObj, function(el){ return el.ORG_CODE })
+	
+	// For each final grouped ORG_CODE, further group by folder name
 	for (var indexT in grpORGT) {
-		var insides = grpORGT[indexT];
 		
+		var insides = grpORGT[indexT]
 		var insGrp = _.groupBy(insides, function(el){ 
-
-			var myFolder = '';
+			var myFolder = ''
 			for (var index in el) {
-				myFolder = index;
+				myFolder = index
 			}
-			return myFolder;
-		});
+			return myFolder
+		})
 
-		var insElem = {};
+		var insElem = {}
 		insElem.ORG_CODE = insides[0].ORG_CODE
 
-		
+		// For each grouped folder, fill insides of the document
 		for(var attributename in insGrp){
 
 			insElem[attributename] = {}
-
+			
 			for(var i=0; i<insGrp[attributename].length; i++) {
-				var tempEl = insGrp[attributename][i];
+				
+				var tempEl = insGrp[attributename][i]
 				
 				for(var attributenameT in tempEl[attributename]){
 					if(attributenameT != 'ORG_CODE') {
@@ -56,20 +58,15 @@ exports.loadJSON = function (options) {
 							tempEl[attributename][attributenameT]
 
 					}
-						
-
-
 				}
-
-
 			}
 		}
 
+		// Add main, aggregated ORG_CODE object to final document
 		finalObj.push(insElem)
-		
-
 	}
 
-	return finalObj;
+	// Return final document
+	return finalObj
 
 }
