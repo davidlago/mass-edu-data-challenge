40d95f1dcd06b237468aed324e4842cbef33d060
David Lago <davidlago@gmail.com>
2014-05-31 16:35:04 -0400

    finished loader function

--- import/loader.js
+++ import/loader.js
@@ -1,41 +1,75 @@
 var fs  = require("fs")
 var path = require("path")
+var _ = require('underscore');
 
 exports.loadJSON = function (options) {
 
 	var obj = JSON.parse(fs.readFileSync(options.path, 'utf8'))
+	var intObj = [];
+	var finalObj = [];
 
-	for (var index in obj) {
+	var filename = path.basename(options.path, '.json')
+	
 
-		var element = obj[index]
-		var base = {}
-		base.ORG_CODE = element.ORG_CODE
-		base[options.folder] = {}
-		var filename = path.basename(options.path, '.json')
-		base[options.folder][filename] = element
+	var grpORG = _.groupBy(obj, function(el){ return el.ORG_CODE; });
 
+	for (var index in grpORG) {
 
-
-
-		var exists = false;
-		options.collection.findOne({ORG_CODE: base.ORG_CODE}, function(err, document) {
-			if(!err) {
-				exists = true;
-			}
-		});
-
-		if(exists) {  // ORG_CODE/folder/filename already exists, use array
-
-
-		} else { // Insert new one
-
-			
-
-		}
-
-
-		console.log(base)
+		var item = grpORG[index];		
+		var tempObj = {};
+		tempObj.ORG_CODE = item[0].ORG_CODE;
+		tempObj[options.folder] = {}
+		tempObj[options.folder][filename] = item
+		intObj.push(tempObj);
 
 	}
+
+	intObj = _.union(intObj,options.state)
+
+	var grpORGT = _.groupBy(intObj, function(el){ return el.ORG_CODE; });
+	for (var indexT in grpORGT) {
+		var insides = grpORGT[indexT];
+		
+		var insGrp = _.groupBy(insides, function(el){ 
+
+			var myFolder = '';
+			for (var index in el) {
+				myFolder = index;
+			}
+			return myFolder;
+		});
+
+		var insElem = {};
+		insElem.ORG_CODE = insides[0].ORG_CODE
+
+		
+		for(var attributename in insGrp){
+
+			insElem[attributename] = {}
+
+			for(var i=0; i<insGrp[attributename].length; i++) {
+				var tempEl = insGrp[attributename][i];
+				
+				for(var attributenameT in tempEl[attributename]){
+					if(attributenameT != 'ORG_CODE') {
+						insElem[attributename][attributenameT] = 
+							tempEl[attributename][attributenameT]
+
+					}
+						
+
+
+				}
+
+
+			}
+		}
+
+		finalObj.push(insElem)
+		
+
+	}
+
+	return finalObj;
 
 }
