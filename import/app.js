e81ac385862fd910d67266f712eef40469b82557
Christopher Michael Buck <butterwell@gmail.com>
2014-06-01 14:45:26 -0400

    record changes

--- import/app.js
+++ import/app.js
@@ -12,24 +12,16 @@ MongoClient.connect("mongodb://localhost:27017/massedu", function(err, db) {
 	var collection = db.collection('org_code')
 	var baseDir = '../data'
 	var dirList = []
-	
-	fs.readdir(baseDir, function (err, files) {
-	    if (err) { throw err }
 
-	    // Make sure we don't process a new folder until the current one is done and inserted
-    	function processFile(index) {
-    		var file = files[index];
-    		if(fs.lstatSync(baseDir+'/'+file).isDirectory() &&
-	    			file !== 'tools'
-	    			 && file !== 'educators_teachers'
-	    			 && file !== 'financial'
-	    			 && file !== 'mcas'
-	    			 && file !== 'students'
-	    			) {
-    				console.log('PROCESSING FOLDER: ' + file)
-		        	walker.walk_and_load(file,function(endState) {
-						console.log('DONE PROCESSING FOLDER: ' + file);
-
+	var do_not_process = ["tools"]
+	// var do_not_process ["tools", "educators_teachers", "financial", "mcas", "students"]
+    				 
+	walker.walk_and_load({
+	  dirBase: baseDir
+  , collection: collection
+  , do_not_process: do_not_process
+  })
+  /*
 						// Insert in collection
 						endState.forEach(function(myState) {
 
@@ -62,5 +54,5 @@ MongoClient.connect("mongodb://localhost:27017/massedu", function(err, db) {
     	processFile(0)
 
 	})
-
+  */
 })
--- import/walker.js
+++ import/walker.js
@@ -14,51 +14,34 @@ var options = {
 
 var state =  []
 
-exports.walk_and_load = function(dirBase,store) {
+exports.walk_and_load = function(options) {
+  var dirBase = options.dirBase
 
   // Wipe out state
   state = [] 
 
   console.log("walk_and_load")
-  var walk_path = path.normalize(__dirname+"/../data/"+dirBase)
+  var walk_path = path.normalize(__dirname+"/"+dirBase)
   console.log(walk_path)
   var walker = walk.walk(walk_path, options)
 
   walker.on("file", function (path, fileStats, next) {
     var tail = fileStats.name.substring(fileStats.name.length-4,fileStats.name.length)
     if (tail == ".csv") {
-      var folder = path.split("/")
-      //console.log(folder[folder.length-1], path+"/"+fileStats.name)    
       var fileroot = fileStats.name.split(".csv")[0]
 
       parse_and_load({
-        folder: folder[folder.length-1]
-      , fileroot: fileroot
-      , filename: path+'/'+fileStats.name
+        path: path
+      , name: fileStats.name
+      , collection: options.collection
       }, next)
       
     } else {
       next()
     }
-/*
-    if (tail == ".json") {
-      var folder = path.split("/")
-      console.log(folder[folder.length-1], path+"/"+fileStats.name)
-    
-      loader.loadJSON({
-        folder: folder
-      , filename: path+'/'+fileStats.name
-      , collection: collection
-      })
-      next()
-    } else {
-      next()
-    }
-*/
   })
 
   walker.on("end", function() {
-    store(state)
   })
   
   walker.on("errors", function (root, nodeStatsArray, next) {
@@ -75,7 +58,8 @@ var param_tsv = {
 
 function parse_and_load(options, next) {
   //console.log(options)
-  var test_input = fs.openSync(options.filename, "r")
+  var filename = options.path+"/"+options.name
+  var test_input = fs.openSync(filename, "r")
   var chunk = new Buffer(80)
   var number_read = fs.readSync(test_input,chunk,0,80,0)
   fs.close(test_input)
@@ -87,21 +71,35 @@ function parse_and_load(options, next) {
     //console.log("No tab")
     //console.log(chunk.toString())
   }
-  var input = fs.createReadStream(options.filename, { encoding: 'utf8' })
+  var input = fs.createReadStream(filename, { encoding: 'utf8' })
   
   var converter=new Converter(param);
 
   //end_parsed will be emitted once parsing finished")
   converter.on("end_parsed",function(jsonObj){
     console.log(_.size(jsonObj)); //here is your result json object
-    state = loader.loadJSON({
-        folder: options.folder
-      , fileroot: options.fileroot
+    var inserts = loader.loadJSON({
+        path: options.path
+      , name: options.name
       , json: jsonObj
-      , state: state
+      , state: []
     })
-    console.log("Size of state: ",_.size(state))
-    next()
+    var size_of_inserts = _.size(inserts)
+    console.log("Size of inserts: ",size_of_inserts)
+    if (size_of_inserts != 0) {
+      //options.collection.update(searchParm, {$set: insertParm}, {upsert:true}
+      options.collection.insert(inserts, {w: 1}, function(err, records){
+		    //console.log(err)
+		    if (records) {
+           console.log(records.length, "records saved")
+        } else {
+           console.log("0 records saved")
+        }
+        next()
+		  })
+		} else {
+		  next()
+		}
   });
 
   var x = 0
