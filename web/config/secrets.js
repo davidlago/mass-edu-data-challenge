7b55ab7f7ad0848429ab1f0c7350b2130ced6337
David Lago <davidlago@gmail.com>
2014-06-03 20:58:04 -0400

    Added facebook connection

--- web/config/secrets.js
+++ web/config/secrets.js
@@ -47,8 +47,8 @@ module.exports = {
   },
 
   facebook: {
-    clientID: process.env.FACEBOOK_ID || '754220301289665',
-    clientSecret: process.env.FACEBOOK_SECRET || '41860e58c256a3d7ad8267d3c1939a4a',
+    clientID: process.env.FACEBOOK_ID || '312840662215037',
+    clientSecret: process.env.FACEBOOK_SECRET || '415ab348c8d23dc6b7ccdfedd05cc044',
     callbackURL: '/auth/facebook/callback',
     passReqToCallback: true
   },
