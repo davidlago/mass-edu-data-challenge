c6112c2e32adb84df5199039aa30407799a8b895
David Lago <davidlago@gmail.com>
2014-06-04 12:33:19 -0400

    Typeahead only starts at 3rd character, temporary fix, makes it faster

--- web/public/js/main.js
+++ web/public/js/main.js
@@ -3,23 +3,20 @@ $(document).ready(function() {
 	var substringMatcher = function(strs) {
 	  return function findMatches(q, cb) {
 	    var matches, substringRegex;
-	 
-	    // an array that will be populated with substring matches
+	 	
 	    matches = [];
-	 
-	    // regex used to determine if a string contains the substring `q`
-	    substrRegex = new RegExp(q, 'i');
-	 
-	    // iterate through the pool of strings and for any string that
-	    // contains the substring `q`, add it to the `matches` array
-	    $.each(strs, function(i, str) {
-	      if (substrRegex.test(str.org_code)) {
-	        // the typeahead jQuery plugin expects suggestions to a
-	        // JavaScript object, refer to typeahead docs for more info
-	        matches.push({ value: str.org_code });
-	      }
-	    });
-	 
+
+	    if(q.length >= 3) { // Only search from 3rd character entered, makes it faster
+		 
+		    substrRegex = new RegExp(q, 'i');
+
+		    $.each(strs, function(i, str) {
+		    	// CHANGE ORG_CODE FOR ORG_NAME WHEN READY!!
+		      if (substrRegex.test(str.org_code)) {
+		        matches.push({ value: str.org_code });
+		      }
+		    });
+	 	}
 	    cb(matches);
 	  };
 	};
