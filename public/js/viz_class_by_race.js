c1f3e8cacc6ac41ea4251a3c4b599eb822d1acd3
David Lago <davidlago@gmail.com>
2014-06-08 21:10:14 -0400

    Added mock viz, using D3.js

--- public/js/viz_class_by_race.js
+++ public/js/viz_class_by_race.js
@@ -13,9 +13,17 @@ var viz_class_by_race = function(container, school, viz, api_url) {
   	// Query the school
   	$.getJSON( api_url + "schools/" + school + "?realm=class_size_by_race", function(school) {
 	 
+      var AFRAME = []
+      var ASIAN = []
+      var HISPANIC = []
+      var WHITE = []
+      var NATAME = []
+      var NAT_HPI = []
+      var MULTI = []
+      var YEARS = []
 
       for(i in school) {
-        cont.append('<p><strong>Year: ' + school[i].year + '</strong></p>');
+        YEARS.push(school[i].year)
         for(j in school[i].class_size_by_race) {
           var COURSE_SUBJECT_NAME = school[i].class_size_by_race[j].COURSE_SUBJECT_NAME
           if (COURSE_SUBJECT_NAME === "All"){
@@ -28,20 +36,80 @@ var viz_class_by_race = function(container, school, viz, api_url) {
             var RACE_NAT_HPI_PCT = school[i].class_size_by_race[j].RACE_NAT_HPI_PCT
             var RACE_MULTI_RACE_PCT = school[i].class_size_by_race[j].RACE_MULTI_RACE_PCT
 
-            cont.append('<p>' + RACE_AFRAME_PCT + '% afro-american students</p>')
-            cont.append('<p>' + RACE_ASIAN_PCT + '% asian students</p>')
-            cont.append('<p>' + RACE_HISPANIC_PCT + '% hispanic students</p>')
-            cont.append('<p>' + RACE_WHITE_PCT + '% white students</p>')
-            cont.append('<p>' + RACE_NATAME_PCT + '% native american students</p>')
-            cont.append('<p>' + RACE_NAT_HPI_PCT + '% hawaiian/pacific islander students</p>')
-            cont.append('<p>' + RACE_MULTI_RACE_PCT + '% multi-race students</p>')
-            
-
+            AFRAME.push(parseFloat(RACE_AFRAME_PCT))
+            ASIAN.push(parseFloat(RACE_ASIAN_PCT))
+            HISPANIC.push(parseFloat(RACE_HISPANIC_PCT))
+            WHITE.push(parseFloat(RACE_WHITE_PCT))
+            NATAME.push(parseFloat(RACE_NATAME_PCT))
+            NAT_HPI.push(parseFloat(RACE_NAT_HPI_PCT))
+            MULTI.push(parseFloat(RACE_MULTI_RACE_PCT))
           }
 
         }
 
       }
+    
+      // define dimensions of graph
+      var m = [80, 80, 80, 80]; // margins
+      var w = 1000 - m[1] - m[3]; // width
+      var h = 400 - m[0] - m[2]; // height
+      
+      // Log in the console the arrays we are about to plot
+      var data = AFRAME
+      console.log(YEARS)
+      console.log(AFRAME)
+      console.log(ASIAN)
+      console.log(HISPANIC)
+      console.log(WHITE)
+      console.log(NATAME)
+      console.log(NAT_HPI)
+      console.log(MULTI)
+
+      // X scale will fit all values from data[] within pixels 0-w
+      var x = d3.scale.linear().domain([0, data.length]).range([0, w]);
+      // Y scale will fit values from 0-10 within pixels h-0 (Note the inverted domain for the y-scale: bigger is up!)
+      var y = d3.scale.linear().domain([0, 10]).range([h, 0]);
+        // automatically determining max range can work something like this
+        // var y = d3.scale.linear().domain([0, d3.max(data)]).range([h, 0]);
+
+      // create a line function that can convert data[] into x and y points
+      var line = d3.svg.line()
+        // assign the X function to plot our line as we wish
+        .x(function(d,i) { 
+          return x(i); 
+        })
+        .y(function(d) { 
+          return y(d); 
+        })
+
+        // Add an SVG element with the desired dimensions and margin.
+        var graph = d3.select("#"+container).append("svg:svg")
+              .attr("width", w + m[1] + m[3])
+              .attr("height", h + m[0] + m[2])
+            .append("svg:g")
+              .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
+
+        // create yAxis
+        var xAxis = d3.svg.axis().scale(x).tickSize(-h).tickSubdivide(true);
+        // Add the x-axis.
+        graph.append("svg:g")
+              .attr("class", "x axis")
+              .attr("transform", "translate(0," + h + ")")
+              .call(xAxis);
+
+
+        // create left yAxis
+        var yAxisLeft = d3.svg.axis().scale(y).ticks(4).orient("left");
+        // Add the y-axis to the left
+        graph.append("svg:g")
+              .attr("class", "y axis")
+              .attr("transform", "translate(-25,0)")
+              .call(yAxisLeft);
+        
+        // Add the line by appending an svg:path element with the data line we created above
+        // do this AFTER the axes above so that the line is above the tick-lines
+        graph.append("svg:path").attr("d", line(data));
+
 
   	});
 
